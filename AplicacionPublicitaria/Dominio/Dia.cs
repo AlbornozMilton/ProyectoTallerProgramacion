4ff849e8afc03c459a22ccc0ca072ccd05bb2d55
Milton <Milton@DESKTOP-P6G4O5Q>
2017-08-17 11:35:34 -0300

    Nuevas Clases Creadas

---
+++ AplicacionPublicitaria/Dominio/Dia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionPublicitaria.Dominio
+{
+    class Dia
+    {
+        private string iNombreDia;
+        private List<RangoHorario> iListaHorarios;
+
+        //CONSTRUCTOR
+        public Dia(string pNombreDia)
+        {
+            this.iNombreDia = pNombreDia;
+            //this.iListaHorarios = new List<RangoHorario>();  <--VER
+        }
+        //PROPIEDADES
+        public string NombreDia
+        {
+            get { return this.iNombreDia; }
+        }
+
+        //METODOS
+    }
+}
---
+++ AplicacionPublicitaria/Dominio/RangoHorario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionPublicitaria.Dominio
+{
+    class RangoHorario
+    {
+        private DateTime iHoraInicio;
+        private DateTime iHoraFin;
+
+        //CONSTRUCTOR
+        public RangoHorario(DateTime pHoraInicio, DateTime pHoraFin)
+        {
+            this.iHoraInicio = pHoraInicio;
+            this.iHoraFin = pHoraFin;
+        }
+        //PROPIEDADES
+        public DateTime HoraInicio
+        {
+            get { return this.iHoraInicio; }
+        }
+        public DateTime HoraFin
+        {
+            get { return this.iHoraFin; }
+        }
+        //METODOS
+    }
+}
