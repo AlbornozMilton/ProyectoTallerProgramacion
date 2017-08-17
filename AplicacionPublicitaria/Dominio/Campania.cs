ee9715443ef7fab468703a28d7596fdf0045ea84
Dell <Dell@DELL-PC>
2017-08-17 11:35:52 -0300

    Imagen, Campania

---
+++ AplicacionPublicitaria/Dominio/Campania.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionPublicitaria.Dominio
+{
+    class Campania
+    {
+        private int iIdCampania;
+        private String iNombre;
+        private float iDuracion;
+        private int iCantCiclos;
+        private List<Imagen> iListaImagenes;
+
+        //constructor de la campania
+        public Campania()
+        {
+            this.iListaImagenes = new List<Imagen>();
+        }
+
+
+    }
+}
---
+++ AplicacionPublicitaria/Dominio/Imagen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionPublicitaria.Dominio
+{
+    class Imagen
+    {
+        private String iNombre;
+        private int iNumSec;
+        private int iTiempo;
+        
+
+
+
+    }
+}
